6bb8a245a5a3e2ee3482b2301c881a077e9bca2a
Madya Santosa <madyasantosa@192.168.1.16>
2022-04-06 17:41:24 +0800

    update-data-produk-seller

--- src/components/product/MerchantProductModal.js
+++ src/components/product/MerchantProductModal.js
@@ -50,17 +50,13 @@ export default function MerchantProductModal({showMerchantProductModal, setShowM
       <>
         {showMerchantProductModal ? (
           <>
-            <div
-              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-white bg-opacity-30"
-            >
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-white bg-opacity-30">
               <div className="relative w-auto my-6 mx-auto">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   {/*header*/}
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                    <h3 className="text-xl font-semibold mt-1">
-                      Tambah Produk
-                    </h3>
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-gray rounded-t">
+                    <h3 className="text-xl font-semibold mt-1">Ubah Produk</h3>
                     <button
                       className="p-1 ml-auto bg-transparent border-0 text-black opacity-50 float-right text-2xl leading-none font-semibold outline-none focus:outline-none"
                       onClick={handleClearInput}
@@ -69,38 +65,62 @@ export default function MerchantProductModal({showMerchantProductModal, setShowM
                     </button>
                   </div>
                   {/*body*/}
-                  <div className="md:w-[50vw] w-[90vw]">
-                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                        <div className="flex gap-10 items-center">
-                            <div className="w-full">
-                                <div className="mb-4">
-                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                                        Harga
-                                    </label>
-                                    <input onChange={(e) => setPrice(e.target.value)} value={price} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="number" />
-                                </div>
-                            </div>
-                            <div className="w-full">
-                                <div className="mb-4">
-                                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                                        Keterangan
-                                    </label>
-                                    <textarea onChange={(e) => setDescription(e.target.value)} value={description} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                                      {description}
-                                    </textarea>
-                                </div>
-                                <div className="mb-4">
-                                    <input checked={status} onChange={(e) => setStatus(e.target.checked)} id="username" type="checkbox" />
-                                    <label className="text-gray-700 text-sm font-bold mx-2" htmlFor="username">
-                                    Aktif
-                                    </label>
-                                </div>
-                            </div>
+                  <div className="md:w-[40vw] w-[90vw]">
+                    <form className="bg-white  rounded px-8 pt-6 pb-8 mb-4">
+                      <div className=" gap-10 items-center">
+                        <div className="w-full">
+                          <div className="mb-4">
+                            <label
+                              className="block text-gray-700 text-sm font-medium mb-2"
+                              htmlFor="username"
+                            >
+                              Harga
+                            </label>
+                            <input
+                              onChange={(e) => setPrice(e.target.value)}
+                              value={price}
+                              className=" appearance-none border border-gray rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-primary"
+                              id="username"
+                              type="number"
+                            />
+                          </div>
                         </div>
+                        <div className="w-full">
+                          <div className="mb-4">
+                            <label
+                              className="block text-gray-700 text-sm font-medium mb-2"
+                              htmlFor="username"
+                            >
+                              Keterangan
+                            </label>
+                            <textarea
+                              onChange={(e) => setDescription(e.target.value)}
+                              value={description}
+                              className=" appearance-none border border-gray rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-primary"
+                            >
+                              {description}
+                            </textarea>
+                          </div>
+                          <div className="mb-4">
+                            <input
+                              checked={status}
+                              onChange={(e) => setStatus(e.target.checked)}
+                              id="username"
+                              type="checkbox"
+                            />
+                            <label
+                              className="text-gray-700 text-sm font-normal mx-2"
+                              htmlFor="username"
+                            >
+                              Aktif
+                            </label>
+                          </div>
+                        </div>
+                      </div>
                     </form>
                   </div>
                   {/*footer*/}
-                  <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <div className="flex items-center justify-end p-6 border-t border-solid border-gray rounded-b">
                     <button
                       className="text-red-500 background-transparent font-bold px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
@@ -109,7 +129,7 @@ export default function MerchantProductModal({showMerchantProductModal, setShowM
                       Tutup
                     </button>
                     <button
-                      className="bg-button text-white active:bg-emerald-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      className="bg-button text-white font-medium text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
                       onClick={handleSubmitProduct}
                     >
--- src/pages/product/MerchantProduct.js
+++ src/pages/product/MerchantProduct.js
@@ -25,108 +25,153 @@ const MerchantProduct = () => {
     const {data, isLoading, error} = useFetch(`/sellers/${merchantId}/items?keyword=${keyword}&offset=${offset}&limit=${limit}`, changes);
 
     return (
-        <>
+      <>
         <Layout>
-            <div className="bg-secondary pl-5 pr-2 pb-3 w-[84vw]">
-                <div className="flex items-center gap-2">
-                    <Link to={'/produk/seller'}><IoMdArrowRoundBack className="text-xl" /></Link>
-                    <h1 className="text-xl py-3 font-semibold">Data Produk Seller</h1>
-                </div>
-                <div className="bg-white h-fit px-3 overflow-x-auto">
-                    <div className="text-center py-3">
-                        <h1 className="text-xl font-semibold">{merchantName}</h1>
-                    </div>
-                        {isLoading && <Loading />}
-                    <hr />
-                        <Limit setLimit={setLimit} limit={limit} setPage={setPage} setKeyword={setKeyword} />
-
-                    <div className="w-full">
-                    <div className="flex flex-col">
-                            <div className="sm:-mx-6 lg:-mx-8">
-                                <div className="inline-block py-2 min-w-full sm:px-6 lg:px-8">
-                                    <div className="overflow-hidden shadow-md sm:rounded-lg">
-                                        <table className="min-w-full">
-                                            <thead className="bg-primary text-white">
-                                                <tr>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        No
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Nama Produk
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Harga
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Keterangan 
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Status
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Action
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                            {error ?  
-                                                        <tr>
-                                                            <td><Error error={"Data Tidak Ditemukan"} /></td>
-                                                        </tr> 
-                                                        
-                                                        :
-                                                data?.merchant_item.map((item, i) => (
-                                                    <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
-                                                            {i + 1 + offset}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            {item.name}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            {item.price}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            {item.description}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            {item.status ? <p>Aktif</p> : <p>Non Aktif</p>}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            <div className="flex gap-3">
-                                                                <button onClick={() => {setIdProduct(item.id); setShowMerchantProductModal(true)}} className="text-button"><FiEdit /></button>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <Pagination
-                            page={page}
-                            setPage={setPage}
-                            limit={limit}
-                            totalData={data?.total}
-                        />
-                
-                </div>
+          <div className="shadow-md px-[32px] pb-[32px] w-full">
+            <div className="flex items-center gap-2">
+              <Link to={"/produk/seller"}>
+                <IoMdArrowRoundBack className="text-xl" />
+              </Link>
+              <h1 className="text-xl py-3 font-semibold">Data Produk Seller</h1>
             </div>
-            <MerchantProductModal 
-                    showMerchantProductModal={showMerchantProductModal} 
-                    setShowMerchantProductModal={setShowMerchantProductModal}
-                    IdProduct={idProduct}
-                    setIdProduct={setIdProduct}
-                    merchantId={merchantId}
-                    setChanges={setChanges} 
-                />
-            </Layout>
-        </> 
-     );
+            <div className="bg-white h-fit px-3 ">
+              <div className="text-center py-3">
+                <h1 className="text-xl font-semibold">{merchantName}</h1>
+              </div>
+              {isLoading && <Loading />}
+              <hr className="border-gray" />
+              <Limit
+                setLimit={setLimit}
+                limit={limit}
+                setPage={setPage}
+                setKeyword={setKeyword}
+              />
+
+              <div className="w-full overflow-auto">
+                <div className="flex flex-col">
+                  <div className="sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+                      <div className="overflow-hidden shadow-md sm:rounded-lg">
+                        <table className="min-w-full">
+                          <thead className="bg-grey900 text-white">
+                            <tr>
+                              <th
+                                scope="col"
+                                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                              >
+                                No
+                              </th>
+                              <th
+                                scope="col"
+                                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                              >
+                                Nama Produk
+                              </th>
+                              <th
+                                scope="col"
+                                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                              >
+                                Harga
+                              </th>
+                              <th
+                                scope="col"
+                                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                              >
+                                Keterangan
+                              </th>
+                              <th
+                                scope="col"
+                                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                              >
+                                Status
+                              </th>
+                              <th
+                                scope="col"
+                                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                              >
+                                Action
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {error ? (
+                              <tr>
+                                <td>
+                                  <Error error={"Data Tidak Ditemukan"} />
+                                </td>
+                              </tr>
+                            ) : (
+                              data?.merchant_item.map((item, i) => (
+                                <tr
+                                  key={item.id}
+                                  className="bg-white odd:bg-tableOdd"
+                                >
+                                  <td className="py-2 px-6 text-md font-medium text-gray-900 whitespace-nowrap">
+                                    {i + 1 + offset}
+                                  </td>
+                                  <td className="py-2 px-6 text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                    {item.name}
+                                  </td>
+                                  <td className="py-2 px-6 text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                    {item.price}
+                                  </td>
+                                  <td className="py-2 px-6 text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                    {item.description}
+                                  </td>
+                                  <td className="py-2 px-6 text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                    {item.status ? (
+                                      <p className="bg-success text-center py-1 px-2 text-white rounded-full">
+                                        Active
+                                      </p>
+                                    ) : (
+                                      <p className="bg-red text-center p-1 px-2 text-white rounded-full">
+                                        Non Active
+                                      </p>
+                                    )}
+                                  </td>
+                                  <td className="py-2 px-6 text-md text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                    <div className="flex justify-center">
+                                      <button
+                                        onClick={() => {
+                                          setIdProduct(item.id);
+                                          setShowMerchantProductModal(true);
+                                        }}
+                                        className="text-white bg-warning text-lg p-[4px] rounded"
+                                      >
+                                        <FiEdit />
+                                      </button>
+                                    </div>
+                                  </td>
+                                </tr>
+                              ))
+                            )}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Pagination
+                page={page}
+                setPage={setPage}
+                limit={limit}
+                totalData={data?.total}
+              />
+            </div>
+          </div>
+          <MerchantProductModal
+            showMerchantProductModal={showMerchantProductModal}
+            setShowMerchantProductModal={setShowMerchantProductModal}
+            IdProduct={idProduct}
+            setIdProduct={setIdProduct}
+            merchantId={merchantId}
+            setChanges={setChanges}
+          />
+        </Layout>
+      </>
+    );
 }
  
 export default MerchantProduct;
