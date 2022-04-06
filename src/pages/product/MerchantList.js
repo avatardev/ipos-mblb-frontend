ddbf1962a73a20dd3a4bde53b3ec94b1cde7d578
Madya Santosa <madyasantosa@192.168.1.16>
2022-04-06 17:18:35 +0800

    update produk seller UI

--- src/pages/product/MerchantList.js
+++ src/pages/product/MerchantList.js
@@ -6,105 +6,146 @@ import Limit from "../../components/utility/Limit";
 import Loading from "../../components/utility/Loading";
 import Pagination from "../../components/utility/Pagination";
 import useFetch from "../../services/useFetch";
-import {BsBoxArrowInDownRight} from "react-icons/bs";
+import { AiOutlineEye } from "react-icons/ai";
 
 const MerchantList = () => {
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [keyword, setKeyword] = useState("");
+  const offset = (page - 1) * limit;
 
-    const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(10);
-    const [keyword, setKeyword] = useState('');
-    const offset = (page - 1) * limit;
-  
-    const {data, isLoading, error} = useFetch(`/sellers?limit=${limit}&offset=${offset}&keyword=${keyword}`);
+  const { data, isLoading, error } = useFetch(
+    `/sellers?limit=${limit}&offset=${offset}&keyword=${keyword}`
+  );
 
-    return (
-        <>
-        <Layout>
-            <div className="bg-secondary pl-5 pr-2 pb-3 w-[84vw]">
-                <div className="flex items-center gap-2">
-                    <h1 className="text-xl py-3 font-semibold">Data Seller</h1>
-                </div>
-                <div className="bg-white h-fit px-3 overflow-x-auto">
-                    {isLoading && <Loading />}
-                    <hr />
-                    <Limit setLimit={setLimit} limit={limit} setPage={setPage} setKeyword={setKeyword} />
+  return (
+    <>
+      <Layout>
+        <div className=" shadow-md px-[32px] pb-[32px] ">
+          <div className="flex items-center gap-2">
+            <h1 className="text-xl font-medium pt-[32px] pb-[24px]">
+              Data Seller
+            </h1>
+          </div>
+          <div className="bg-white rounded-lg h-fit px-3 shadow-lg pt-3">
+            {isLoading && <Loading />}
+            <Limit
+              setLimit={setLimit}
+              limit={limit}
+              setPage={setPage}
+              setKeyword={setKeyword}
+            />
 
-                    <div className="w-full">
-                    <div className="flex flex-col">
-                            <div className="sm:-mx-6 lg:-mx-8">
-                                <div className="inline-block py-2 min-w-full sm:px-6 lg:px-8">
-                                    <div className="overflow-hidden shadow-md sm:rounded-lg">
-                                        <table className="min-w-full">
-                                            <thead className="bg-primary text-white">
-                                                <tr>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        No
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Perusahaan
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Alamat
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Email 
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                        Telp
-                                                    </th>
-                                                    <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                                            Action
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                            {error ?  
-                                                        <tr>
-                                                            <td><Error error={"Data Tidak Ditemukan"} /></td>
-                                                        </tr> 
-                                                        
-                                                        :
-                                                    data?.seller.map((item, i) => (
-                                                    <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
-                                                            {i + 1 + offset}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            <Link to={`/produk/seller/${item.id}/${item.company}`}>
-                                                                {item.company}
-                                                            </Link>
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            {item.address}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            {item.emali}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                            {item.phone}
-                                                        </td>
-                                                        <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                                <div className="flex gap-3">
-                                                                    <Link to={`/produk/seller/${item.id}/${item.company}`}><BsBoxArrowInDownRight className="text-button" /></Link>
-                                                                </div>
-                                                         </td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <div className="w-full overflow-auto">
+              <div className="flex flex-col">
+                <div className="sm:-mx-6 lg:-mx-8">
+                  <div className="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden shadow-md sm:rounded-lg">
+                      <table className="min-w-full">
+                        <thead className="bg-grey900 text-white">
+                          <tr>
+                            <th
+                              scope="col"
+                              className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                            >
+                              No
+                            </th>
+                            <th
+                              scope="col"
+                              className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                            >
+                              Perusahaan
+                            </th>
+                            <th
+                              scope="col"
+                              className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                            >
+                              Alamat
+                            </th>
+                            <th
+                              scope="col"
+                              className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                            >
+                              Email
+                            </th>
+                            <th
+                              scope="col"
+                              className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                            >
+                              Telp
+                            </th>
+                            <th
+                              scope="col"
+                              className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                            >
+                              Action
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {error ? (
+                            <tr>
+                              <td colSpan={8} className="py-5 px-3">
+                                <Error error={"Data Tidak Ditemukan"} />
+                              </td>
+                            </tr>
+                          ) : (
+                            data?.seller.map((item, i) => (
+                              <tr
+                                key={item.id}
+                                className="bg-white odd:bg-tableOdd"
+                              >
+                                <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                                  {i + 1 + offset}
+                                </td>
+                                <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                  <Link
+                                    to={`/produk/seller/${item.id}/${item.company}`}
+                                  >
+                                    {item.company}
+                                  </Link>
+                                </td>
+                                <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                  {item.address}
+                                </td>
+                                <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                  {item.emali}
+                                </td>
+                                <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                  {item.phone}
+                                </td>
+                                <td className="py-4 px-6 text-sm whitespace-nowrap ">
+                                  <div className="flex justify-center">
+                                    <Link
+                                      to={`/produk/seller/${item.id}/${item.company}`}
+                                      className="text-white bg-success text-lg p-[4px] rounded"
+                                    >
+                                      <AiOutlineEye />
+                                    </Link>
+                                  </div>
+                                </td>
+                              </tr>
+                            ))
+                          )}
+                        </tbody>
+                      </table>
                     </div>
-
-                    <Pagination page={page} setPage={setPage} limit={limit} totalData={data?.total} />
-                
+                  </div>
                 </div>
+              </div>
             </div>
-            </Layout>
-        </> 
-     );
-}
+
+            <Pagination
+              page={page}
+              setPage={setPage}
+              limit={limit}
+              totalData={data?.total}
+            />
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
  
 export default MerchantList;
